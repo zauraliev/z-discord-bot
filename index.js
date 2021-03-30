983d14cc6ec4e61e6732f7036cb7487a20bf8a9b
ZAUR ALIYEV <zaur_aliev@hotmail.com>
2021-03-30 00:20:15 +0000

    Added dotenv and wolfram-alpha-api for testing the questions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
+const WolframAlphaAPI = require("wolfram-alpha-api");
+// const waApi = WolframAlphaAPI(`${process.env.WOLFRAM_APPID}`);
 const fetch = require("node-fetch");
 const client = new Discord.Client();
-
+require("dotenv").config();
 
 function getQuote() {
   return fetch("https://zenquotes.io/api/random")
@@ -13,13 +15,38 @@ function getQuote() {
     })
 }
 
+function zBotAnswer(question) {
+  return fetch(`http://api.wolframalpha.com/v2/query?input=${question}&appid=${process.env.WOLFRAM_APPID}`)
+  .then(res=>{
+    return res.json()
+  })
+  .then(data => {
+    return data
+  })
+}
+
 
 client.on("ready", () => {
-  console.log(`logged in ${client.user.tag}`);
+  console.log(`logged in ${client.user.username}`);
 });
 
 client.on("message", msg => {
   if(msg.content === "ping") msg.reply("pong");
+  if(msg.mentions.has(client.user)) {
+    msg.reply("Hey there what's up!");
+    let question = `${msg.content}`
+    .replace(`<@${client.user.id}>`,'')
+    if(question.trim()) {
+      msg.reply(`${question.trim()} to you!`)
+    }
+  }
+  if(msg.mentions.has(client.user) && msg.content.includes("hello")) {
+    msg.reply("Thanks for for saying Hello!")
+  }
+
+
+
+
   switch(msg.content.toUpperCase()) {
         case '?RST':
             resetBot(msg.channel);
