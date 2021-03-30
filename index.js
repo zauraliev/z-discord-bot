1f7545978ea3d0c22d4d6b72b70ca9d3af806974
Zaur <zaur_aliev@hotmail.com>
2021-03-30 02:36:30 -0400

    Improved Bot answers with short answer api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
-const WolframAlphaAPI = require("wolfram-alpha-api");
-// const waApi = WolframAlphaAPI(`${process.env.WOLFRAM_APPID}`);
 const fetch = require("node-fetch");
 const client = new Discord.Client();
+const axios = require("axios");
 require("dotenv").config();
+const user = JSON.parse(process.env.JSON);
 
 function getQuote() {
   return fetch("https://zenquotes.io/api/random")
@@ -16,43 +16,62 @@ function getQuote() {
 }
 
 function zBotAnswer(question) {
-  return fetch(`http://api.wolframalpha.com/v2/query?input=${question}&appid=${process.env.WOLFRAM_APPID}`)
-  .then(res=>{
-    return res.json()
-  })
-  .then(data => {
-    return data
-  })
-}
+  console.log(question)
+  
+  const baseUrl = 'http://api.wolframalpha.com/v1/result?';
+  
+  let body = {
+    appid: process.env.WOLFRAM_APPID,
+    i: question
+  }
+  
+  let encodedUrl = convertToEncodedUrl(body);
 
+  return axios.get(`${baseUrl}${encodedUrl}`)
+    .then(res => {
+      console.log(res.data)
+      return res.data;
+    }).catch(error => {
+      console.log(error.message)
+      return "Sorry can't answer that";
+    })
+
+}
 
 client.on("ready", () => {
   console.log(`logged in ${client.user.username}`);
+  console.log(user[0].USERNAME)
 });
 
 client.on("message", msg => {
-  if(msg.content === "ping") msg.reply("pong");
-  if(msg.mentions.has(client.user)) {
-    msg.reply("Hey there what's up!");
-    let question = `${msg.content}`
-    .replace(`<@${client.user.id}>`,'')
-    if(question.trim()) {
-      msg.reply(`${question.trim()} to you!`)
+  const userRegex = /\<[@!].*?\>/g;
+
+  if (msg.content === 'ping') msg.reply('pong');
+  
+  if (msg.mentions.has(client.user)) {
+    
+    msg.reply(`Hey there what's up? I am the ${user[1].NAME} :)!`);
+
+    let question = `${msg.content}`.replace(userRegex, '');
+    
+    if (question.trim()) {
+      zBotAnswer(`${question}`).then(data => {
+        msg.reply(`${data}!`);
+      }).catch(error => {
+        msg.reply(`${error}!`);
+      })
     }
   }
-  if(msg.mentions.has(client.user) && msg.content.includes("hello")) {
-    msg.reply("Thanks for for saying Hello!")
+  if(msg.mentions.has(client.user) && msg.content.toLowerCase().includes("hello")) {
+    msg.reply('Thanks for saying Hello!')
   }
-
-
-
 
   switch(msg.content.toUpperCase()) {
         case '?RST':
             resetBot(msg.channel);
             break;
   }
-  if(msg.content === "$inspire") {
+  if(msg.content === '$inspire') {
     getQuote().then(res => {
       msg.reply(res);
     })
@@ -64,7 +83,17 @@ client.login(process.env.TOKEN)
 // Turn bot off (destroy), then turn it back on
 function resetBot(channel) {
     // send channel a message that you're resetting bot [optional]
-    channel.send(`Resetting... ${client.user}`)
+    channel.send(`Resetting...`)
     .then(() => client.destroy())
     .then(() => client.login(process.env.TOKEN));
+}
+
+function convertToEncodedUrl(obj) {
+  if (obj && Object.keys(obj).length !== 0) {
+    return Object.keys(obj)
+              .map(key => key + '=' + obj[key])
+              .join('&');
+  } else {
+    return '';
+  }
 }
