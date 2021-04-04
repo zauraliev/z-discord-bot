40f3177b3cfa74b2c648ce5a11b2f4d444caa83f
Zaur <zaur_aliev@hotmail.com>
2021-04-03 23:02:06 -0400

    Added services for db connection and functions

---
+++ services.js
@@ -0,0 +1,12 @@
+const Database = require("@replit/database");
+const db = new Database();
+
+const addInspire = inspire => {
+  // Add inspire into DB
+  console.log(inspire);
+}
+
+const deleteInspire = inspireId => {
+  // Delete inspire by id from DB
+  console.log(inspireId);
+}
