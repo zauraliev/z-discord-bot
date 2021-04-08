57adf83981d2abd5fe4fb9150555b03bcbaa8d23
Zaur <zaur_aliev@hotmail.com>
2021-04-08 03:15:36 -0400

    Added Update and Delete motivations with DB integrations

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js");
+const Database = require("@replit/database")
+
+
 const client = new Discord.Client();
+const db = new Database()
 
 const axios = require("axios");
 require("dotenv").config();
@@ -7,11 +11,47 @@ const user = JSON.parse(process.env.JSON);
 
 const sadWords = ["sad", "depressed", "unhappy", "angry"];
 
-const motivations = [
+const initMotivations = [
   "Cheer up!",
   "Be motivated!",
   "Hang in there!"
 ];
+
+db.get("motivations").then(motivations => {
+  if(!motivations || motivations.length < 1){
+    db.set("motivations", initMotivations);
+    console.log(motivations)
+  }
+  console.log(motivations)
+})
+
+
+function updateMotivation(motivationMSG) {
+  db.get("motivations").then(motivations =>{
+    motivations.push(motivationMSG);
+    db.set("motivations", motivations)
+      .then(() => {
+        db.get("motivations").then(motivations => {
+          console.log(motivations)
+        })
+      })
+  })
+}
+
+function deleteMotivation(index) {
+  db.get("motivations").then(motivations => {
+    if(motivations.length > index) {
+      motivations.splice(index, 1)
+      db.set("motivations", motivations)
+        .then(() => {
+          db.get("motivations").then(motivations => {
+            console.log(motivations)
+          })
+        })
+    }
+  })
+}
+
 
 function getQuote() {
   
@@ -57,8 +97,22 @@ client.on("message", msg => {
   if (msg.content === 'ping') msg.reply('pong');
 
   if (sadWords.some(word => msg.content.includes(word))) {
-    const motivation = motivations[Math.floor(Math.random() * motivations.length)];
-    msg.reply(motivation);
+    db.get("motivations").then(motivations => {
+      const motivation = motivations[Math.floor(Math.random() * motivations.length)];
+      msg.reply(motivation);
+    })
+  }
+
+  if(msg.content.startsWith("$new")) {
+    motivationMSG = msg.content.split("$new ")[1];
+    updateMotivation(motivationMSG);
+    msg.channel.send("New motivational message added...")
+  }
+
+  if(msg.content.startsWith("$del")) {
+    index = msg.content.split("$del ")[1];
+    deleteMotivation(index);
+    msg.channel.send("New motivational message deleted...")
   }
   
   if (msg.mentions.has(client.user)) {
